7146aed7879524398a0db4eac0ef73ad44f71d9d
Nasfa24 <podcastmiliksaya@gmail.com>
2026-03-07 01:50:28 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Konfigurasi Project Firebase KONEKTA (Matur Suwon)
+// IMPORT APP CHECK (Keamanan tak kasat mata reCAPTCHA v3)
+import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-check.js";
+
+// Konfigurasi Project Firebase KONEKTA
 const firebaseConfig = {
     apiKey: "AIzaSyA2nV-dKXEl-5ui0WQienkFiQ-XfugDzPE",
     authDomain: "matur-suwon.firebaseapp.com",
@@ -17,8 +20,14 @@ const firebaseConfig = {
 // Inisialisasi Firebase App
 const app = initializeApp(firebaseConfig);
 
+// INISIALISASI APP CHECK (Menggunakan Site Key KONEKTA)
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LdoHIIsAAAAAHffNopoxKMz_FtFZ0FpPHPCUgiE'),
+  isTokenAutoRefreshEnabled: true // Otomatis perbarui token agar relawan tidak tiba-tiba logout
+});
+
 // Inisialisasi Firestore Database
 const db = getFirestore(app);
 
-// Export db agar bisa dipakai di index.html, member.html, dan gm.html
+// Export db agar bisa dipakai di halaman lain (index, member, gm)
 export { db };
